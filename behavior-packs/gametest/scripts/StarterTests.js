ca2604e30ac7309fa9713a4c51d12ff0ef55907b
Mark Wiemer <markwiemer@outlook.com>
2022-09-17 18:21:37 -0700

    Add registration code
    https://learn.microsoft.com/en-us/minecraft/creator/documents/gametestbuildyourfirstgametest#gametest-registration

--- behavior-packs/gametest/scripts/StarterTests.js
+++ behavior-packs/gametest/scripts/StarterTests.js
@@ -1,1 +1,6 @@
-// Hello world!
+// Registration code for our test
+GameTest.register("StarterTests", "simpleMobTest", simpleMobTest)
+  .maxTicks(410)
+  .structureName(
+    "startertests:mediumglass" // use the mediumglass.mcstructure file
+  );
