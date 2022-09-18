bf4106ab6c4e41cadf52347507698600d9023dc2
Mark Wiemer <markwiemer@outlook.com>
2022-09-17 18:31:17 -0700

    Add glass pen structure

--- behavior-packs/gametest/scripts/StarterTests.js
+++ behavior-packs/gametest/scripts/StarterTests.js
@@ -1,6 +1,24 @@
+import * as GameTest from "mojang-gametest";
+import { BlockLocation } from "mojang-minecraft";
+
+function simpleMobTest(test) {
+  const attackerId = "fox";
+  const victimId = "chicken";
+
+  test.spawn(attackerId, new BlockLocation(5, 2, 5));
+  test.spawn(victimId, new BlockLocation(2, 2, 2));
+
+  test.assertEntityPresentInArea(victimId, true);
+
+  // Succeed when the victim dies
+  test.succeedWhen(() => {
+    test.assertEntityPresentInArea(victimId, false);
+  });
+}
+
 // Registration code for our test
 GameTest.register("StarterTests", "simpleMobTest", simpleMobTest)
   .maxTicks(410)
   .structureName(
-    "startertests:mediumglass" // use the mediumglass.mcstructure file
+    "startertests:glasspen" // use the glasspen.mcstructure file
   );
