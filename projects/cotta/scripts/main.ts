10480d2352dd6195d5e424bc9fc879b42c8b9cbf
Mark Wiemer <markwiemer@outlook.com>
2022-09-19 12:29:43 -0700

    Add some initialization code
    Replace main.ts with basic script that runs several commands
    
    https://learn.microsoft.com/en-us/minecraft/creator/documents/scriptinggettingstarted#add-some-initialization-code

--- projects/cotta/scripts/main.ts
+++ projects/cotta/scripts/main.ts
@@ -1,13 +1,37 @@
 import { world } from "mojang-minecraft";
 
-let tickIndex = 0;
+const START_TICK = 100;
 
-function mainTick() {
-  tickIndex++;
+// global variables
+let curTick = 0;
 
-  if (tickIndex === 100) {
-    world.getDimension("overworld").runCommand("say Hello starter!");
-  }
+function initializeBreakTheTerracotta() {
+  const overworld = world.getDimension("overworld");
+
+  // catch in case we've already added this score before.
+  try {
+    overworld.runCommand('scoreboard objectives add score dummy "Level"');
+  } catch (e) {}
+
+  // eliminate pesky nearby mobs
+  try {
+    overworld.runCommand("kill @e[type=!player]");
+  } catch (e) {}
+
+  overworld.runCommand("scoreboard objectives setdisplay sidebar score");
+  overworld.runCommand("give @p diamond_sword");
+  overworld.runCommand("give @p dirt 64");
+
+  overworld.runCommand("scoreboard players set @p score 0");
+  overworld.runCommand("say BREAK THE TERRACOTTA!");
 }
 
-world.events.tick.subscribe(mainTick);
+function gameTick() {
+  if (curTick === START_TICK) {
+    initializeBreakTheTerracotta();
+  }
+
+  curTick++;
+}
+
+world.events.tick.subscribe(gameTick);
