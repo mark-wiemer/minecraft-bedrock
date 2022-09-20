d28bffc43c1c81d7094a9e513ee31b4737d5e77b
Mark Wiemer <markwiemer@outlook.com>
2022-09-19 17:18:13 -0700

    User for loop to access iterator values
    Also add some traces woo

--- projects/zombieWar/scripts/main.ts
+++ projects/zombieWar/scripts/main.ts
@@ -1,37 +1,45 @@
 import { BlockLocation, Dimension, EntityQueryOptions, Player, world } from "mojang-minecraft";
 
+const name = "Zombie War";
+
 let tickIndex = 0;
+/** Ticks per second */
+const seconds = 20;
+const startTick = 5 * seconds;
 const spawn = new BlockLocation(0, -59, 0);
+const debug = true;
 
 const locToString = (loc: Pick<BlockLocation, "x" | "y" | "z">): string => [loc.x, loc.y, loc.z].join(" ");
-
-const initialize = (overworld: Dimension, player: Player) => {
-  const command = `tp ${player.name} ${locToString(spawn)}`;
-  say(`${command}`);
-  overworld.runCommand(command);
-};
 
 const say = (x: string): void => {
   world.getDimension("overworld").runCommand(`say ${x}`);
 };
 
+/** Say if in debug mode, else do nothing */
+const trace = (x: string): void => {
+  if (debug) say(x);
+};
+
+const initialize = (overworld: Dimension, player: Player) => {
+  say(`Welcome to ${name}!`);
+  const command = `tp ${player.name} ${locToString(spawn)}`;
+  overworld.runCommand(command);
+};
+
 const mainTick = () => {
   tickIndex++;
 
-  if (tickIndex === 100) {
+  if (tickIndex === startTick) {
+    trace(`${name} starting up...`);
     const overworld = world.getDimension("overworld");
     let player1: Player | null = null;
     try {
-      say(`Getting player name`);
-      const iterator = overworld.getPlayers();
-      say(`got all players`);
-      const result = iterator.next(); // this is failing
-      say(`got all player 1 result`);
-      player1 = result.value as Player;
-      say(`got player name ${player1.name}`);
+      const playerIterator = overworld.getPlayers({ closest: 1 } as unknown as EntityQueryOptions);
+      for (const player of playerIterator) {
+        player1 = player;
+      }
     } catch {
-      say(`couldn't get name :(`);
-      say(`maybe there are no players found?`);
+      say(`${name} crashed while finding nearest player to spawn`);
     }
     if (player1) initialize(overworld, player1);
   }
