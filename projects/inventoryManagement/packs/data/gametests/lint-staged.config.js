7b8af90edbd0d52ab19a899c453815bb7a10f223
Mark Wiemer <markwiemer@outlook.com>
2022-10-16 11:36:52 -0700

    Fixup lint-staged steps

---
+++ projects/inventoryManagement/packs/data/gametests/lint-staged.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export default {
+    // Check formatting for the files we're given
+    '*': 'npm run format',
+    // If any source file changes, validate all source files for simplicity
+    'src/*': (files) => {
+        return `npm run quality_glob ${files.join(
+            ' ',
+        )} && tsc -p tsconfig.json && npm run testonce`;
+    },
+    'package.json': 'npm run packagejson',
+};
