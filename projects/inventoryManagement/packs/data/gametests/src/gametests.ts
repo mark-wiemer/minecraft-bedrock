35de64bcd0489caba74d6c87b2812d2d8a134cdb
Mark Wiemer <markwiemer@outlook.com>
2022-10-16 11:08:56 -0700

    Move gametests to new file

---
+++ projects/inventoryManagement/packs/data/gametests/src/gametests.ts
@@ -0,0 +1,26 @@
+import * as GameTest from 'mojang-gametest';
+
+// Gametests use mojang-minecraft since they depend on Minecraft functionality
+import { BlockLocation } from 'mojang-minecraft';
+
+type Spec = (arg: GameTest.Test) => void;
+
+const simpleMobTest: Spec = (test: GameTest.Test) => {
+    const attackerId = 'fox';
+    const victimId = 'chicken';
+
+    test.spawn(attackerId, new BlockLocation(3, 2, 3));
+    test.spawn(victimId, new BlockLocation(2, 2, 2));
+
+    test.assertEntityPresentInArea(victimId, true);
+
+    // Succeed when the victim dies
+    test.succeedWhen(() => {
+        test.assertEntityPresentInArea(victimId, false);
+    });
+};
+
+// Registration Code for our test
+GameTest.register('StarterTests', 'simpleMobTest', simpleMobTest)
+    .maxTicks(410)
+    .structureName('gametests:test_5x5');
--- projects/inventoryManagement/packs/data/gametests/src/main.ts
+++ projects/inventoryManagement/packs/data/gametests/src/main.ts
@@ -1,26 +1,4 @@
 import { world } from 'mojang-minecraft';
-import * as GameTest from 'mojang-gametest';
-import { BlockLocation } from 'mojang-minecraft';
 import { onTickEvent } from './utils';
 
 world.events.tick.subscribe((tickEvent) => onTickEvent(tickEvent, world));
-
-function simpleMobTest(test: GameTest.Test) {
-    const attackerId = 'fox';
-    const victimId = 'chicken';
-
-    test.spawn(attackerId, new BlockLocation(3, 2, 3));
-    test.spawn(victimId, new BlockLocation(2, 2, 2));
-
-    test.assertEntityPresentInArea(victimId, true);
-
-    // Succeed when the victim dies
-    test.succeedWhen(() => {
-        test.assertEntityPresentInArea(victimId, false);
-    });
-}
-
-// Registration Code for our test
-GameTest.register('StarterTests', 'simpleMobTest', simpleMobTest)
-    .maxTicks(410)
-    .structureName('gametests:test_5x5');
